08b8186d11f742a0548ba64c6d73febfcc54cb6c
Belaalcantara <isabelaalcantara.senai564@gmail.com>
2024-05-08 08:58:16 -0300

    feat: finalizacao do crud e testes

--- db/script.sql
+++ db/script.sql
@@ -1,6 +1,7 @@
-/*criacao das tabelas jogadores e combate*/
+/*criacao database, tabelas players e combat*/
+create database jogosvorazes;
 
-CREATE TABLE jogadores (
+CREATE TABLE players (
 id SERIAL PRIMARY KEY,
  nome VARCHAR(100) NOT NULL,
  distrito VARCHAR(100) NOT NULL,
@@ -9,27 +10,27 @@ id SERIAL PRIMARY KEY,
  vida INT NOT NULL
  );
 
-  CREATE TABLE combate(
+  CREATE TABLE  combat(
  id SERIAL PRIMARY KEY,
- jogadores1_id INT NOT NULL,
- jogadores2_id INT NOT NULL,
+ players1_id INT NOT NULL,
+ players2_id INT NOT NULL,
  ganhador_id INT NOT NULL,
- FOREING KEY (jogadores1_id) REFERENCES jogadores(id),
- FOREING KEY (jogadores2_id) REFERENCES jogadores(id),
+ FOREING KEY (players1_id) REFERENCES players(id),
+ FOREING KEY (players2_id) REFERENCES players(id),
  );
 
 
-/*criacao dos inserts dos jogadores que iram batalhar*/
+/*criacao dos inserts dos players que iram batalhar*/
 
-INSERT INTO jogadores(nome, distrito, arma, nivel, vida) VALUES ('Finnick Odair', 'distrito 4', 'tridente', 90, 77);
-INSERT INTO jogadores(nome, distrito, arma, nivel, vida) VALUES ('Coriolanus Snow', 'Capital', 'veneno',  85, 90);
-INSERT INTO jogadores(nome, distrito, arma, nivel, vida) VALUES ('Katniss Everdeen', 'distrito 12', 'arco e flecha', 100, 1000);
-INSERT INTO jogadores(nome, distrito, arma, nivel, vida) VALUES ('Peeta Mellark', 'distrito 12', 'manipulação', 100, 99);
-INSERT INTO jogadores(nome, distrito, arma, nivel, vida) VALUES ('Gale Hawthorne', 'distrito 2', 'arma de fogo',70, 80);
-INSERT INTO jogadores(nome, distrito, arma, nivel, vida) VALUES ('Lucy Gray Baird', 'distrito 12', 'cobras',90, 70);
-INSERT INTO jogadores(nome, distrito, arma, nivel, vida) VALUES ('Boggs', 'distrito 13', 'arma de fogo',70, 0);
-INSERT INTO jogadores(nome, distrito, arma, nivel, vida) VALUES ('Effie Trinket', 'capital', 'conhecimento',70, 73);
-INSERT INTO jogadores(nome, distrito, arma, nivel, vida) VALUES ('Haymitch Abernathy', 'distrito 12', 'faca',60, 80);
-INSERT INTO jogadores(nome, distrito, arma, nivel, vida) VALUES ('Presidente Alma Coin', 'distrito 13', 'armas nucleares',80, 0);
-INSERT INTO jogadores(nome, distrito, arma, nivel, vida) VALUES ('Gloss', 'distrito 1', 'punhal', 80,0 );
-INSERT INTO jogadores(nome, distrito, arma, nivel, vida) VALUES ('Thresh', 'distrito 11', 'foice', 60,0 );
+INSERT INTO players(nome, distrito, arma, nivel, vida) VALUES ('Finnick Odair', 'distrito 4', 'tridente', 90, 77);
+INSERT INTO players(nome, distrito, arma, nivel, vida) VALUES ('Coriolanus Snow', 'Capital', 'veneno',  85, 90);
+INSERT INTO players(nome, distrito, arma, nivel, vida) VALUES ('Katniss Everdeen', 'distrito 12', 'arco e flecha', 100, 1000);
+INSERT INTO players(nome, distrito, arma, nivel, vida) VALUES ('Peeta Mellark', 'distrito 12', 'manipulação', 100, 99);
+INSERT INTO players(nome, distrito, arma, nivel, vida) VALUES ('Gale Hawthorne', 'distrito 2', 'arma de fogo',70, 80);
+INSERT INTO players(nome, distrito, arma, nivel, vida) VALUES ('Lucy Gray Baird', 'distrito 12', 'cobras',90, 70);
+INSERT INTO players(nome, distrito, arma, nivel, vida) VALUES ('Boggs', 'distrito 13', 'arma de fogo',70, 0);
+INSERT INTO players(nome, distrito, arma, nivel, vida) VALUES ('Effie Trinket', 'capital', 'conhecimento',70, 73);
+INSERT INTO players(nome, distrito, arma, nivel, vida) VALUES ('Haymitch Abernathy', 'distrito 12', 'faca',60, 80);
+INSERT INTO players(nome, distrito, arma, nivel, vida) VALUES ('Presidente Alma Coin', 'distrito 13', 'armas nucleares',80, 0);
+INSERT INTO players(nome, distrito, arma, nivel, vida) VALUES ('Gloss', 'distrito 1', 'punhal', 80,0 );
+INSERT INTO players(nome, distrito, arma, nivel, vida) VALUES ('Thresh', 'distrito 11', 'foice', 60,0 );
---
+++ index.js
@@ -0,0 +1,197 @@
+const express = require("express");
+const { Pool } = require("pg");
+
+const app = express();
+const PORT = 3000;
+
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "postgres",
+  password: "ds564",
+  port: 7007,
+});
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send(`<h3>Seja bem-vindo à seleção de tributos!</h3>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrNb1_ISy7jZWT1btczvTSssWppojODpbkkFdMDfGl0A&s" alt="jogos vorazes" width="200" height="100">
+    `);
+});
+
+app.post("/players", async (req, res) => {
+    const { nome, distrito, arma, nivel, vida} = req.body;
+    try {
+
+      await pool.query(
+        "INSERT INTO players (nome, distrito, arma, nivel, vida) VALUES ($1, $2, $3, $4, $5)",
+        [nome, distrito, arma, nivel, vida]
+      );
+      res.json({
+        status: "🎉",
+        message: "tributo criado com sucesso",
+      });
+    } catch (error) {
+      console.error("Erro ao criar tributo", error);
+      res.status(500).send({
+        status: "error",
+        message: "Erro ao criar tributo",
+      });
+    }
+  });
+
+app.get("/players", async (req, res) => {
+  try {
+    const result = await pool.query(`
+        SELECT * FROM players;
+      `);
+    res.json({
+      status: "🎉",
+      message: "Lista de tributos",
+      quantity: result.rowCount,
+      players: result.rows,
+    });
+  } catch (error) {
+    console.error("Erro ao buscar tributos", error);
+    res.status(500).send({
+      status: "error",
+      message: "Erro ao buscar tributos",
+    });
+  }
+});
+
+app.get("/players/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
+      const result = await pool.query("SELECT * FROM players WHERE id = $1", [id]);
+      if (result.rowCount === 0) {
+        res.json({
+          status: "error",
+          message: `tributo com id ${id} não encontrado`,
+        });
+      }
+      res.json({
+        message: "tributo:",
+        players: result.rows[0],
+      });
+    } catch (error) {
+      console.error("Erro ao buscar players", error);
+    }
+  });
+
+  app.put("/players/:id", async (req, res) => {
+    try {
+      const { nome, distrito, arma, nivel, vida } = req.body;
+      const { id } = req.params;
+      await pool.query(
+        "UPDATE players SET nome = $1, distrito = $2, arma = $3, nivel = $4, vida = $5 WHERE id = $6",
+        [nome, distrito, arma, nivel, vida, id]
+      );
+      res.json({
+        status: "🎉",
+        message: "tributo atualizado com sucesso",
+      });
+    } catch (error) {
+      console.error("Erro ao atualizar tributo", error);
+      res.status(500).send({
+        status: "error",
+        message: "Erro ao atualizar tributo",
+      });
+    }
+  });
+
+  
+/*app.get("/players/nome/:nome", async (req, res) => {
+    try {
+      const { nome } = req.params;
+      const result = await pool.query(
+        "SELECT * FROM players WHERE LOWER(nome) $1",
+        [`%${nome.toLowerCase()}%`]
+      );
+      if (result.rowCount === 0) {
+        res.json({
+          status: "error",
+          message: `tributo com nome ${nome} não encontrado`,
+        });
+      }
+      res.json({
+        status: "🎉",
+        message: "tributo encontrado",
+        players: result.rows,
+      });
+    } catch (error) {
+      console.error("Erro ao buscar tibuto", error);
+      res.status(500).send({
+        status: "error",
+        message: "Erro ao buscar tributo",
+      });
+    }
+  });
+  */
+  
+  app.delete("/players/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await pool.query("DELETE FROM players WHERE id = $1", [id]);
+    if (result.rowCount === 0) {
+      res.json({
+        status: "error",
+        message: `tributo com id ${id} não encontrado`,
+      });
+    }
+    res.json({
+      status: "success",
+      message: "tributo deletado com sucesso",
+    });
+  } catch (error) {
+    console.error("Erro ao deletar tributo", error);
+    res.status(500).send({
+      status: "error",
+      message: "Erro ao deletar tributo",
+    });
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Que os jogos comecem🏹: ${PORT}`);
+  }).on('error', (error) => {
+    console.error('Erro ao iniciar o servidor:', error.message);
+  });
+
